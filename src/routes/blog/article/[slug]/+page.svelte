e715388d7f66bf1778fa2be0b596181e4acb4bdb
Logan <thecrazykid2121@gmail.com>
2024-09-10 10:59:36 -0400

    Blog done

--- src/routes/blog/article/[slug]/+page.svelte
+++ src/routes/blog/article/[slug]/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 // @ts-nocheck
-    import umla_creation from '../content/umla-creation.svelte';
+    import markdownit from 'markdown-it'
+    import { onMount } from 'svelte';
 
     import { page } from '$app/stores';  
     let path = $page.url.pathname  
@@ -18,6 +19,29 @@
     if (slugParts[1] === "creation") {
         tags = "creation";
     }
+    let content = "Loading Article...";
+
+    onMount(() => {
+        loadArticle();
+    });
+
+    async function loadArticle() {
+        const res = await fetch("/articles/" + data.slug + ".md");
+        if (res.status === 404) {
+            content = "Article Not Found";
+            return;
+        }
+        const mdContent = await res.text();
+        console.log(mdContent);
+        content = md.render(mdContent);
+    }
+
+
+    const md = markdownit({
+        html: true,
+        linkify: true,
+        typographer: true
+    });
 </script>
 
     <div class="flex justify-center mt-10">
@@ -32,8 +56,12 @@
         <p class="text-sm text-blue-800">{article.date}</p>
     </div>
 
-    <div class="flex justify-center ml-6">
-        <p class="text-sm text-blue-800">Tags: {tags}</p>
+    <div class="flex justify-center ml-6 mt-4">
+        <p class="text-sm text-blue-500">Tag: <span class="text-sm text-blue-300 bg-slate-600 p-2 rounded-md">{tags}</span></p>
     </div>
 
-    <svelte:component this={umla_creation} />
+<div class="flex justify-center mt-10 mx-20 py-2 rounded-md shadow-md h-full">
+    <div class="mx-44 flex flex-col [&_h1]:text-4xl [&_h2]:text-2xl [&_h3]:text-xl [&_h4]:text-lg [&_p]:text-base [&_h1]:text-blue-400 [&_h1]:font-bold [&_a]:text-blue-500 [&_a]:hover:underline [&_h2]:text-blue-400 [&_li]:ml-4 [&_li]:list-disc [&_blockquote]:text-gray-500 [&_blockquote]:dark:text-gray-400">
+        {@html content}
+    </div>
+</div>
---
+++ static/articles/site-creation.md
@@ -0,0 +1,15 @@
+# Why I created this site
+> Why did I do it??
+
+I created this site as a home to all of my project, as well as to host information about me!
+
+## Technologies involved
+- Svelte
+- NodeJS
+- TypeScript
+- Markdown
+
+## Challenges I faced throughout the project
+- One major challenge I had with this project, is I'm pretty bad at styling components, and actually piecing things together with CSS.  This project was an excuse for me to get very familiar with everything possible with tailwind, and get my information down.
+
+- Another major challenge I had was coming up with a blog system. I went for a markdown approach where the articles are imported as markdown, then loaded as html.
